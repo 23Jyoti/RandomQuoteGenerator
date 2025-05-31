576eb69f300eb9738bbfc77f3547806c00840af7
Jyoti Gaud <23jyotigaud@gmail.com>
2025-05-31 11:14:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css'; 
+import QuoteGenerator from './components/QuoteGenerator';
+
+const App = () => {
+  return (
+    <div className="App">
+      <QuoteGenerator />
+    </div>
+  );
+};
+
+export default App;
+
